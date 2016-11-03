431ba0dc709b004227c77108a869379b63ba95c7
gouf <innocent.zero@gmail.com>
2016-11-03 16:38:34 +0900

    年月の移動機能をメソッドに書き出し

--- app/controllers/time_records_controller.rb
+++ app/controllers/time_records_controller.rb
@@ -1,5 +1,6 @@
 class TimeRecordsController < ApplicationController
   before_action :set_time_record, only: [:show, :edit, :update, :destroy]
+  before_action :set_year_month_instance_variable, only: [:index]
 
   # GET /time_records
   # GET /time_records.json
@@ -21,16 +22,6 @@ class TimeRecordsController < ApplicationController
           TimeRecord.find_or_create_by(work_date: date)
         end
       end
-
-    # params[:year], params[:month] 情報から年月の表示を移動する
-    @year, @month =
-      [
-        params[:year].nil? ? Date.today.year : params[:year].to_i,
-        params[:month].nil? ? Date.today.month : params[:month].to_i
-      ]
-    # 月がその範囲を踏み切ったときに年を移動する
-    @month, @year = [12, @year - 1] if @month < 1
-    @month, @year = [1, @year + 1] if @month > 12
 
     @time_records = map_time_records_to_calender.call(@month, @year)
   end
@@ -106,5 +97,30 @@ class TimeRecordsController < ApplicationController
       :work_end_at,
       :description
     )
+  end
+
+  def set_year_month_instance_variable
+    # params[:year], params[:month] 情報から年月の表示を移動する
+    # 次点 session[:year], session[:month] から取り出し
+    # なければ現在の年・月から値を設定
+    @year, @month =
+      [
+        params[:year].nil? ? nil : params[:year].to_i,
+        params[:month].nil? ? nil : params[:month].to_i
+      ]
+
+    @year ||= session[:year]
+    @month ||= session[:month]
+
+    @year ||= Date.today.year
+    @month ||= Date.today.month
+
+    # 月がその範囲を踏み切ったときに年を移動する
+    @month, @year = [12, @year - 1] if @month < 1
+    @month, @year = [1, @year + 1] if @month > 12
+
+    # 値をセッションに保存して再利用する
+    session[:year] = @year
+    session[:month] = @month
   end
 end
