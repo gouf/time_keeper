2adb10cdb05569dd8f47a200cc6fd18c8af10f7a
gouf <innocent.zero@gmail.com>
2016-11-09 18:01:20 +0900

    Revert "work_time_pattern_id 保持カラムの追加"
    This reverts commit 44b669eeb42c8a50731456cfe1c08299794c8bec.

--- db/migrate/20161108121511_add_work_time_pattern_id_association_to_transportation_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddWorkTimePatternIdAssociationToTransportationExpenses < ActiveRecord::Migration[5.0]
-  def change
-    add_column :transportation_expenses, :work_time_pattern_id, :integer
-  end
-end
