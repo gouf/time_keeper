033929fbe9dc3279babd80632987d63af5765aef
gouf <innocent.zero@gmail.com>
2016-11-09 17:56:58 +0900

    値がない場合に - を表示

--- app/helpers/transportation_expenses_helper.rb
+++ app/helpers/transportation_expenses_helper.rb
@@ -30,4 +30,8 @@ module TransportationExpensesHelper
     )
   end
 
+  def is_round_trip(transportation_expense)
+    return nil if transportation_expense.cost.blank?
+    transportation_expense.is_round_trip ? '往復' : '片道'
+  end
 end
