83735aba7bf82219f27ea7f2bf3767dfaedaed1d
gouf <innocent.zero@gmail.com>
2016-11-17 02:25:36 +0900

    交通費パターン 登録処理 着手仮組み

--- app/controllers/transportation_expense_patterns_controller.rb
+++ app/controllers/transportation_expense_patterns_controller.rb
@@ -1,5 +1,7 @@
 class TransportationExpensePatternsController < ApplicationController
   def index
+    # TODO : 入力フォームに複数(増減可変)のTransportationExpense モデルへの入力を受け付ける機能を追加する
+    @transportation_expense_models = [TransportationExpense.new]
   end
 
   def new
